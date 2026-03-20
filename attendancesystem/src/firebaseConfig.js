a7d94bb43ebcb3985aa6e3b71e103ddd6b68c779
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-20 22:35:25 +0300

    Update firebaseConfig.js

--- attendancesystem/src/firebaseConfig.js
+++ attendancesystem/src/firebaseConfig.js
@@ -1,26 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// إعدادات مشروعك من Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyA7qW5bjTPQA0q9XgKd6OHDzaNCDbtz2z0",
   authDomain: "firestore-1488a.firebaseapp.com",
-  databaseURL: "https://firestore-1488a-default-rtdb.firebaseio.com",
   projectId: "firestore-1488a",
-  storageBucket: "firestore-1488a.firebasestorage.app",
+  storageBucket: "firestore-1488a.appspot.com", // الشكل الصحيح للتخزين
   messagingSenderId: "284773652304",
   appId: "1:284773652304:web:f379106224b0570b91fe55",
   measurementId: "G-FZJMJ7RPGT"
 };
 
-
+// تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Firestore لتخزين البيانات النصية
 const db = getFirestore(app);
 
-export { db };
+// Storage لتخزين الصور
+const storage = getStorage(app);
+
+export { db, storage };
