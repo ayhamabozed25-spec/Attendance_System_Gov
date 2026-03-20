8164f438b7d5e26de7031780ebb934b41c48d7d1
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-20 20:10:21 +0300

    Update App.js

--- attendancesystem/src/App.js
+++ attendancesystem/src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+
+import React, { useState } from "react";
 import Register from "./Register";
 import Attendance from "./Attendance";
 
 function App() {
+  const [activePage, setActivePage] = useState(null);
+
   return (
     <div>
       <h1>نظام تسجيل الحضور بالوجه</h1>
-      <Register />
-      <Attendance />
+
+      {/* الأزرار */}
+      <button onClick={() => setActivePage("register")}>تسجيل جديد</button>
+      <button onClick={() => setActivePage("attendance")}>تسجيل حضور</button>
+
+      {/* عرض المكونات حسب الزر المضغوط */}
+      {activePage === "register" && <Register />}
+      {activePage === "attendance" && <Attendance />}
     </div>
   );
 }
