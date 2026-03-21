1f827c44569d1b22f99123ea976108a503bb4471
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-21 03:00:56 +0300

    Update Attendance.js

--- attendancesystem/src/Attendance.js
+++ attendancesystem/src/Attendance.js
@@ -37,7 +37,7 @@ function Attendance() {
       setInterval(async () => {
         if (modelsLoaded) {
           const detections = await faceapi
-            .detectAllFaces(videoRef.current)
+            .detectSingleFace(videoRef.current)
             .withFaceLandmarks()
             .withFaceDescriptors();
 
@@ -68,7 +68,7 @@ function Attendance() {
 
   const recognizeFace = async () => {
     const detections = await faceapi
-      .detectAllFaces(videoRef.current)
+      .detectSingleFace(videoRef.current)
       .withFaceLandmarks()
       .withFaceDescriptor();
     if (detections) {
