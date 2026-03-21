b9fb21147fbff15a357d4da4f8736ba7ca75d961
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-21 16:32:36 +0300

    Update sw.js

--- attendancesystem/public/sw.js
+++ attendancesystem/public/sw.js
@@ -1,11 +1,13 @@
 const CACHE_NAME = "face-models-cache-v1";
 const urlsToCache = [
 "Attendance_System_Gov/models/ssd_mobilenetv1_model-weights_manifest.json",
-"Attendance_System_Gov/models/ssd_mobilenetv1_model-shard1.bin",
+"Attendance_System_Gov/models/ssd_mobilenetv1_model-shard1",
+ "Attendance_System_Gov/models/ssd_mobilenetv1_model-shard2",
 "Attendance_System_Gov/models/face_landmark_68_model-weights_manifest.json",
- "Attendance_System_Gov/models/face_landmark_68_model-shard1.bin",
+ "Attendance_System_Gov/models/face_landmark_68_model-shard1",
 "Attendance_System_Gov/models/face_recognition_model-weights_manifest.json",
- "Attendance_System_Gov/models/face_recognition_model-shard1.bin"
+ "Attendance_System_Gov/models/face_recognition_model-shard1",
+ "Attendance_System_Gov/models/face_recognition_model-shard2"
 ];
 
 // عند التثبيت، خزّن الملفات في الكاش
