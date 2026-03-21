2bd73939a1916ecae48c741566b74daf82cfede2
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-21 03:22:05 +0300

    Update Attendance.js

--- attendancesystem/src/Attendance.js
+++ attendancesystem/src/Attendance.js
@@ -14,7 +14,7 @@ function Attendance() {
   }, []);
 
   const loadModels = async () => {
-    const MODEL_URL = process.env.PUBLIC_URL + "/models";
+    const MODEL_URL = "/models";
     await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL);
     await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
     await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
