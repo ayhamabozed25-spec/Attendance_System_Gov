ce4e753fa58c1e7d7e8f668712c73ec32318e8a7
ayhamabozed25-spec <ayhamabozed25@gmail.com>
2026-03-21 13:06:08 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "face-models-cache-v1";
+const urlsToCache = [
+  "/models/ssd_mobilenetv1_model-weights_manifest.json",
+  "/models/ssd_mobilenetv1_model-shard1.bin",
+  "/models/face_landmark_68_model-weights_manifest.json",
+  "/models/face_landmark_68_model-shard1.bin",
+  "/models/face_recognition_model-weights_manifest.json",
+  "/models/face_recognition_model-shard1.bin"
+];
+
+// عند التثبيت، خزّن الملفات في الكاش
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// عند أي طلب، حاول أولاً من الكاش ثم من الشبكة
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
